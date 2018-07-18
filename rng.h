e98088f83b1dc78db740bc6cde01685ae7155163
bhimanijanki <bhimani.j@husky.neu.edu>
2018-07-17 21:04:09 -0700

    Update rng.h

--- rng.h
+++ rng.h
@@ -1,10 +1,3 @@
-/* -------------------------------------------------------------------------  
- * Name            : rng.h  (header file for the library file rng.c) 
- * Author          : Steve Park & Dave Geyer  
- * Language        : ANSI C 
- * Latest Revision : 09-11-98
- * ------------------------------------------------------------------------- 
- */
 
 #if !defined( _RNG_ )
 #define _RNG_
